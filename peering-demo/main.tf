10e1668bfcc088ca5862feb33f61cc483e98dc6f
robertdippolito <robert.e.dippolito@gmail.com>
2026-02-14 23:53:16 -0500

    adding new ec2 and peering modules

--- peering-demo/main.tf
+++ peering-demo/main.tf
@@ -44,3 +44,63 @@ module "vpc_b" {
     VPC     = "b"
   }
 }
+
+module "app_a" {
+  source = "../modules/ec2_http"
+
+  name      = "app-a"
+  vpc_id    = module.vpc_a.vpc_id
+  subnet_id = module.vpc_a.public_subnet_id
+
+  endpoint_message = "hello from vpc-a"
+
+  http_ingress_cidrs = ["10.20.0.0/16"]
+
+  tags = {
+    Project = "vpc-peering-demo"
+    Env     = "lab"
+  }
+}
+
+module "app_b" {
+  source = "../modules/ec2_http"
+
+  name      = "app-b"
+  vpc_id    = module.vpc_b.vpc_id
+  subnet_id = module.vpc_b.public_subnet_id
+
+  endpoint_message = "hello from vpc-b"
+
+  http_ingress_cidrs = ["10.10.0.0/16"]
+
+  tags = {
+    Project = "vpc-peering-demo"
+    Env     = "lab"
+  }
+}
+
+module "peering" {
+  source = "../modules/vpc_peering"
+
+  name = "demo-a-b-peering"
+
+  requester_vpc_id   = module.vpc_a.vpc_id
+  accepter_vpc_id    = module.vpc_b.vpc_id
+  requester_vpc_cidr = module.vpc_a.vpc_cidr
+  accepter_vpc_cidr  = module.vpc_b.vpc_cidr
+
+  requester_route_table_ids = [
+    module.vpc_a.public_route_table_id,
+    module.vpc_a.private_route_table_id,
+  ]
+
+  accepter_route_table_ids = [
+    module.vpc_b.public_route_table_id,
+    module.vpc_b.private_route_table_id,
+  ]
+
+  tags = {
+    Project = "vpc-peering-demo"
+    Env     = "lab"
+  }
+}
